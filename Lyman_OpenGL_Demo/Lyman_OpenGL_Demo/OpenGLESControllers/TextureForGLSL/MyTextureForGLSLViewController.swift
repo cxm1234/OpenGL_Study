e3289954c8f40c50a8deaab5f059e13fc5f28973
chengxiaoming <chengxiaoming@top-stack.com>
2022-07-01 19:04:17 +0800

    Lyman texture GLSL

--- Lyman_OpenGL_Demo/Lyman_OpenGL_Demo/OpenGLESControllers/TextureForGLSL/MyTextureForGLSLViewController.swift
+++ Lyman_OpenGL_Demo/Lyman_OpenGL_Demo/OpenGLESControllers/TextureForGLSL/MyTextureForGLSLViewController.swift
@@ -53,6 +53,15 @@ class MyTextureForGLSLViewController: UIViewController {
         
         bindRenderLayer(layer: layer)
         
+        // MARK: 通过GLKTextureLoader 来加载纹理，并存放在 GLKBaseEffect 中
+        guard let imagePath = Bundle.main.path(forResource: "sample", ofType: "jpg") else {
+            assert(false, "error in generate imagePath")
+        }
+        guard let image = UIImage(contentsOfFile: imagePath) else {
+            assert(false, "error in generate image")
+        }
+        
+        
         
     }
 
@@ -71,6 +80,44 @@ extension MyTextureForGLSLViewController {
         context?.renderbufferStorage(Int(GL_RENDERBUFFER), from: layer)
         
         // 将渲染缓存绑定到帧缓存上
+        glGenFramebuffers(1, &frameBuffer)
+        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), frameBuffer)
+        glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_RENDERBUFFER), renderBuffer)
+        
+    }
+    
+    private func createTexture(with image: UIImage) -> GLuint {
+        // 将UIImage 转换为 CGImageRef
+        guard let cgImageRef = image.cgImage else {
+            return GLuint()
+        }
+        let width = GLuint(bitPattern: Int32(cgImageRef.width))
+        let height = GLuint(bitPattern: Int32(cgImageRef.height))
+        let rect = CGRect(x: 0, y: 0, width: Int(width), height: Int(height))
+        
+        
+        // 绘制图片
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let imageDataPointer = UnsafeMutableRawPointer(bitPattern: MemoryLayout<GLuint>.size * Int(width) * Int(height))
+        let context = CGContext(
+            data: imageDataPointer,
+            width: Int(width),
+            height: Int(height),
+            bitsPerComponent: 8,
+            bytesPerRow: Int(width) * MemoryLayout<GLuint>.size,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue
+        )
+        context?.translateBy(x: 0, y: CGFloat(height))
+        context?.scaleBy(x: 1.0, y: -1.0)
+        context?.clear(rect)
+        context?.draw(cgImageRef, in: rect)
+        
+        // 生成纹理
+        var textureID: GLuint = GLuint()
+        glGenTextures(1, &textureID)
+        glBindTexture(GLenum(GL_TEXTURE_2D), textureID)
+//        glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_RGBA, width, <#T##height: GLsizei##GLsizei#>, <#T##border: GLint##GLint#>, <#T##format: GLenum##GLenum#>, <#T##type: GLenum##GLenum#>, <#T##pixels: UnsafeRawPointer!##UnsafeRawPointer!#>)
         
         
         
